69e45b50675018a46a304c9c4d1f0527ad528347
murilopopadiuk <murilo.popadiuk@aluno.senai.br>
2025-10-24 16:20:41 -0300

    Add files via upload

---
+++ Ref-javascript/script.js
@@ -0,0 +1,27 @@
+function calcular(numero1, numero2, resposta, operador) {
+    var num1 = parseFloat(document.getElementById(numero1).value)
+    var num2 = parseFloat(document.getElementById(numero2).value)
+    var resultado
+
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2
+            break;
+        case '-':
+            resultado = num1 - num2
+            break;
+
+            case'*':
+            resultado = num1 * num2
+            break;
+
+
+            case '/':
+                resultado = num1 / num2
+        default: "Operador inválido"
+            break;
+    }
+
+     document.getElementById(resposta).textContent = resultado
+
+}
